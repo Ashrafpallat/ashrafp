23d61a13d984ab41e66efe347cfd542d056bb9a9
Ashrafpallat <ashrafpallat123@gmail.com>
2025-02-15 16:03:04 +0530

    redirecting to home at first

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,18 +5,20 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col bg-[#121212] ">
-      <Navbar/>
-      <div className="container mt-24 mx-auto py-4 px-12">
-        <HeroSection />
-        {/* <AboutSection/> */}
-        {/* <ProjectsSection/> */}
-        {/* <EmailSection/> */}
-      </div>
-      <Footer/>
-    </main>
-  );
+  // return (
+  //   <main className="flex min-h-screen flex-col bg-[#121212] ">
+  //     <Navbar/>
+  //     <div className="container mt-24 mx-auto py-4 px-12">
+  //       <HeroSection />
+  //       {/* <AboutSection/> */}
+  //       {/* <ProjectsSection/> */}
+  //       {/* <EmailSection/> */}
+  //     </div>
+  //     <Footer/>
+  //   </main>
+  // );
+  redirect("/home");
 }
